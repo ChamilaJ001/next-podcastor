7022c1c921863d822409bd7063546bc9af2d9041
Chamila Jayasinghe <official.chamilaj@gmail.com>
2024-06-02 12:32:10 +0530

    Implement Authentication with clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://primary-midge-35.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
